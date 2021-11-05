316b2bab234760f9a94a9f4b8760b7192d2b012a
supervisionuth <93388243+supervisionuth@users.noreply.github.com>
2021-11-05 11:36:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World devops!</p>"
+   return "<p>Hello, World!</p>"
 
+if __name__== "__main__":
 
-if __name__ == "__main__":
-    app.run('0.0.0.0')
+   app.run('0.0.0.0')
